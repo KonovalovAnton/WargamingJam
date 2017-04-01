b7b842ed278834a891aeb3e03a14ca7cb903b154
Anton Konovalov <konovalov.anton.39@gmail.com>
2017-04-01 20:25:42 +0300

    teams & colors

---
+++ Assets/Player/PlayerTeam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeam : MonoBehaviour {
+
+    bool isSad;
+    Color sad = Color.red;
+    Color happy = Color.green;
+
+    public SpriteRenderer body;
+    public PhotonView pv;
+
+    private void Start()
+    {
+        if(pv.isMine)
+        {
+            bool sad = true;
+            if (PhotonNetwork.playerList.Length > 1)
+            {
+                sad = false;
+            }
+            pv.RPC("ChangeMood", PhotonTargets.AllBufferedViaServer, sad);
+        }
+    }
+
+    [PunRPC]
+    public void ChangeMood(bool isSad)
+    {
+        this.isSad = isSad;
+        body.color = isSad ? sad : happy;
+    }
+
+}
