65b2bd33129040526e8e4e6f8165cad5969cfc3f
Anton Konovalov <konovalov.anton.39@gmail.com>
2017-04-01 22:15:50 +0300

    pyatnashki fix

--- Assets/Player/CaptureOthers.cs
+++ Assets/Player/CaptureOthers.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class CaptureOthers : MonoBehaviour {
 
     public PhotonView pv;
+    public PlayerTeam me;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(pv.isMine)
         {
             PlayerTeam p = collision.gameObject.GetComponent<PlayerTeam>();
-            if (p != null && p.IsSad())
+            if (!me.IsSad() && p != null && p.IsSad())
             {
                 p.ChangeMoodRPC(false);
             }
